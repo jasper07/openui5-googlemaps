491b14fba18635cca9a339c8e129fdc21da7bd50
John Patterson <patterjo>
2014-07-04 19:44:50 +1000

    feat visible prop on overlays

--- samples.1/app1/view/Master.controller.js
+++
@@ -1,8 +0,0 @@
-    sap.ui.controller("testapp.view.Master", {
-
-        handleListSelect: function(oEvent) {
-            this.navigation.navTo("idViewRoot--idViewDetail", oEvent.getParameter("listItem").getBindingContext());
-        }
-
-
-    });
