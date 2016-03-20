d3510e47c5384058dfeeb0d10f4500dba0062e91
John P <patterjo@gmail.com>
2016-03-20 23:21:00 +1100

    [UPDATE] added more unit tests

--- test/allTests.js
+++ test/allTests.js
@@ -1,8 +1,8 @@
 sap.ui.define([
     "test/unit/Map", 
     "test/unit/MapUtils",
-    // "test/unit/Marker",
-    // "test/unit/Polygon",
-    // "test/unit/Polyline",
-    // "test/unit/Waypoint"
+    "test/unit/Marker",
+    "test/unit/Polygon",
+    "test/unit/Polyline",
+    "test/unit/Waypoint"
 ], function() {});
