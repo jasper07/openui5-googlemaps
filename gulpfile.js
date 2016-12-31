da32c5c14b0f5f370a5c8581c45df276641cc7c6
John Patterson <john.patterson@secondphase.com.au>
2016-12-31 11:16:48 +1100

    [FIX] broken travis build

--- gulpfile.js
+++ gulpfile.js
@@ -182,8 +182,8 @@ gulp.task('release', ['bump', 'build'], function() {
             'git commit -m "release ' + newVer + '"',
             'git tag ' + newVer,
             'git push',
-            'git push --tags',
-            'git branch -f gh-pages master'
+            'git push --tags'
+            // 'git branch -f gh-pages master'
             // 'git push origin gh-pages', //update branch from master
             // 'git checkout gh-pages',
             // 'git merge master',
